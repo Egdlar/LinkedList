d30f7e345548c5c497dc8fd8de91c6cd93adbf24
Egdlar <94698352+Egdlar@users.noreply.github.com>
2022-10-16 18:19:36 +0800

    Add files via upload

---
+++ Linkedlist.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+
+public class LinkedlistPtask {
+    public static void main(String args[]) {
+      
+      LinkedList Cartoon_And_Anime_Character = new LinkedList();
+      
+      Cartoon_And_Anime_Character.add("Spongedbob");
+      Cartoon_And_Anime_Character.add("Patrick");
+      Cartoon_And_Anime_Character.add("Squidward");
+      Cartoon_And_Anime_Character.add("Luffy");
+      
+      System.out.println("Cartoon and Anime Character List: " + Cartoon_And_Anime_Character);
+      
+      
+      
+      
+    }
+}
